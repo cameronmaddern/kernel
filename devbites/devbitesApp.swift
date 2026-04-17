cb64e240c100bd39281db7f9657692e0d2fd2385
profferamaster <128373717+profferamaster@users.noreply.github.com>
2026-04-17 16:30:32 -0700

    Initial Commit

---
+++ devbites/devbitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  devbitesApp.swift
+//  devbites
+//
+//  Created by Cameron Maddern on 17/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct devbitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ devbitesTests/devbitesTests.swift
@@ -0,0 +1,17 @@
+//
+//  devbitesTests.swift
+//  devbitesTests
+//
+//  Created by Cameron Maddern on 17/4/2026.
+//
+
+import Testing
+@testable import devbites
+
+struct devbitesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
